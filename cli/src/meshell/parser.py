8edea1e0ad9fbd65a8003ce54f2a79df5f34ffbb
jonas-elhs <jonas.elhs@outlook.com>
2025-09-25 16:10:42 +0200

    cli: move toggle option to specific command

--- cli/src/meshell/parser.py
+++ cli/src/meshell/parser.py
@@ -7,21 +7,20 @@ def parse_args() -> (argparse.ArgumentParser, argparse.Namespace):
   parser.add_argument("-v", "--version", action="store_true", help="print the current version")
 
   subcommands = parser.add_subparsers(title="commands", metavar="COMMAND")
-  # subcommands = parser.add_subparsers(title="subcommands", metavar="COMMAND")
 
   # Bar
   bar_parser = subcommands.add_parser("bar", help="control the bar")
-  bar_parser.set_defaults(execute=bar.toggle)
+  bar_subparsers = bar_parser.add_subparsers(title="actions", metavar="ACTION", required=True)
 
-  bar_subparsers = bar_parser.add_subparsers(title="actions", metavar="ACTION")
+  bar_subparsers.add_parser("toggle", help="toggle the bar").set_defaults(execute=bar.toggle)
   bar_subparsers.add_parser("show", help="show the bar").set_defaults(execute=bar.show)
   bar_subparsers.add_parser("hide", help="hide the bar").set_defaults(execute=bar.hide)
 
   # Power Menu
   power_parser = subcommands.add_parser("power", help="control the power menu")
-  power_parser.set_defaults(execute=powermenu.toggle)
-
   power_subparsers = power_parser.add_subparsers(title="actions", metavar="ACTION")
+
+  power_subparsers.add_parser("toggle", help="toggle the power menu").set_defaults(execute=powermenu.toggle)
   power_subparsers.add_parser("show", help="show the power menu").set_defaults(execute=powermenu.show)
   power_subparsers.add_parser("hide", help="hide the power menu").set_defaults(execute=powermenu.hide)
 
@@ -33,8 +32,8 @@ def parse_args() -> (argparse.ArgumentParser, argparse.Namespace):
 
   # Wallpaper
   wallpaper_parser = subcommands.add_parser("wallpaper", help="control the wallpaper")
-
   wallpaper_subparsers = wallpaper_parser.add_subparsers(title="actions", metavar="ACTION", required=True)
+
   wallpaper_subparsers.add_parser("get", help="get current wallpaper").set_defaults(execute=wallpaper.get)
   wallpaper_set_parser = wallpaper_subparsers.add_parser("set", help="set new wallpaper")
   wallpaper_set_parser.set_defaults(execute=wallpaper.set)
